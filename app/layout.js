e428b0d22b47bb2257fc622990a454b67ee97ca8
Malcev-Nikita <nik2004itamalcev@gmail.com>
2023-09-12 00:58:37 +0400

    commit

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-import './globals.css'
+import './css/main.css'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import styles from './page.module.css'
+import styles from './css/main.css'
 
 export default function Home() {
   return (
