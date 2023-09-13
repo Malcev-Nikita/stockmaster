411b070528707603d5468983e659d463c5c660d7
Malcev-Nikita <nik2004itamalcev@gmail.com>
2023-09-14 00:26:09 +0400

    commit

--- app/page.js
+++ app/page.js
@@ -1,5 +1,9 @@
 import Image from 'next/image'
 
+import { BsPencil } from "react-icons/bs";
+import { MdDeleteOutline } from "react-icons/md";
+import { AiOutlinePlusCircle } from "react-icons/ai";
+
 async function getCatalogs() {
   const res = await fetch(process.env.NEXT_PUBLIC_STRAPI_API_URL + '/api/stock-master-catalogs?populate=images')
   return res.json()
@@ -8,18 +12,31 @@ async function getCatalogs() {
 export default async function Page() {
   const catalogs = await getCatalogs()
 
+  function getCatalog(id) {
+    // const res = await fetch(process.env.NEXT_PUBLIC_STRAPI_API_URL + '/api/stock-master-catalogs/:' + id)
+    console.log(1)
+  }
+
   return (
     <main>
       <div className='catalogs'>
         {catalogs.data.map(catalog => {
           return (
             <div className='catalog' key={catalog.id}>
-              <Image src={process.env.NEXT_PUBLIC_STRAPI_API_URL + catalog.attributes.images.data[0].attributes.url} width={600} height={600} alt="test"/>
+              <div className='media_container'>
+                <Image src={process.env.NEXT_PUBLIC_STRAPI_API_URL + catalog.attributes.images.data[0].attributes.url} width={600} height={600} alt="test"/>  
+
+                <button className='edit' onClick={getCatalog(catalog.id)}> <BsPencil/> </button>
+
+                <div className='delete'><MdDeleteOutline/></div>
+              </div>
 
               <h2>{catalog.attributes.name}</h2>
             </div>
           )
         })}
+
+        <div className='catalog last_element'><AiOutlinePlusCircle/></div>
       </div>
     </main>
   )
