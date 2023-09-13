1d7f010cc16499867f59edd78b2d28e964d3a530
Malcev-Nikita <nik2004itamalcev@gmail.com>
2023-09-13 18:50:00 +0400

    commit

--- app/page.js
+++ app/page.js
@@ -1,22 +1,26 @@
 import Image from 'next/image'
 
-async function getContents() {
-  const res = await fetch(process.env.NEXT_PUBLIC_STRAPI_API_URL + '/api/catalogs')
+async function getCatalogs() {
+  const res = await fetch(process.env.NEXT_PUBLIC_STRAPI_API_URL + '/api/stock-master-catalogs?populate=images')
   return res.json()
 }
 
 export default async function Page() {
-  const data = await getContents()
+  const catalogs = await getCatalogs()
 
   return (
     <main>
       <div className='catalogs'>
-        {
-          data.map
-        }
-      </div>
+        {catalogs.data.map(catalog => {
+          return (
+            <div className='catalog' key={catalog.id}>
+              <Image src={process.env.NEXT_PUBLIC_STRAPI_API_URL + catalog.attributes.images.data[0].attributes.url} width={600} height={600} alt="test"/>
 
-      {console.log(data)}
+              <h2>{catalog.attributes.name}</h2>
+            </div>
+          )
+        })}
+      </div>
     </main>
   )
 }
