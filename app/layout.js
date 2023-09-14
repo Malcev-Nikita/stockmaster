7fb0c1aede5b868bdc4874e62c09c4d3ff3b34a5
Malcev Nikita <nik2004itamalcev@gmail.com>
2023-09-14 18:50:54 +0400

    commit

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Header/>
 
-        {children}
+        <main> {children} </main>
         
         <Footer/>
       </body>
--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ import { MdDeleteOutline } from "react-icons/md";
 import { AiOutlinePlusCircle } from "react-icons/ai";
 
 async function getCatalogs() {
-  const res = await fetch(process.env.NEXT_PUBLIC_STRAPI_API_URL + '/api/stock-master-catalogs?populate=images')
+  const res = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_API_URL}/api/stock-master-catalogs?populate=images`)
   return res.json()
 }
 
@@ -20,26 +20,24 @@ export default async function Page() {
   }
 
   return (
-    <main>
-      <div className='catalogs'>
-        {catalogs.data.map(catalog => {
-          return (
-            <div className='catalog' key={catalog.id}>
-              <div className='media_container'>
-                <Image src={process.env.NEXT_PUBLIC_STRAPI_API_URL + catalog.attributes.images.data[0].attributes.url} width={600} height={600} alt="test"/>  
+    <div className='catalogs'>
+      {catalogs.data.map(catalog => {
+        return (
+          <div className='catalog' key={catalog.id}>
+            <div className='media_container'>
+              <Image src={process.env.NEXT_PUBLIC_STRAPI_API_URL + catalog.attributes.images.data[0].attributes.url} width={600} height={600} alt="test"/>  
 
-                <button className='edit' onClick={(e) => getCatalog(catalog.id)}> <BsPencil/> </button>
+              <button className='edit' onClick={(e) => getCatalog(catalog.id)}> <BsPencil/> </button>
 
-                <div className='delete'><MdDeleteOutline/></div>
-              </div>
-
-              <h2>{catalog.attributes.name}</h2>
+              <div className='delete'><MdDeleteOutline/></div>
             </div>
-          )
-        })}
 
-        <div className='catalog last_element'><AiOutlinePlusCircle/></div>
-      </div>
-    </main>
+            <h2>{catalog.attributes.name}</h2>
+          </div>
+        )
+      })}
+
+      <div className='catalog last_element'><AiOutlinePlusCircle/></div>
+    </div>
   )
 }
