0221b6087f1b2c82e68982fe9814ec6e6a336e2c
Malcev-Nikita <nik2004itamalcev@gmail.com>
2023-09-15 19:01:06 +0400

    commit

--- app/[slug]/edit.js
+++ app/[slug]/edit.js
@@ -14,25 +14,55 @@ export default function EditComponent({data}) {
                 <input type='button' id="edit" value={edit ? "Выключить" : "Включить"} onClick={() => setEdit(!edit)}/>
             </div>
 
-            <div className='catalogs__item'>
-                <div className='catalogs__item_left'>
-                    <Image 
-                        src={process.env.NEXT_PUBLIC_STRAPI_API_URL + data.attributes.images.data[0].attributes.url} 
-                        width={600} 
-                        height={600} 
-                        alt={`${data.attributes.name}`}
-                    />             
-                </div>
+            {
+                edit ? (
+                    <div className='catalogs__item'>
+                        <div className='catalogs__item_left'>
+                            <Image 
+                                src={process.env.NEXT_PUBLIC_STRAPI_API_URL + data.attributes.images.data[0].attributes.url} 
+                                width={600} 
+                                height={600} 
+                                alt={`${data.attributes.name}`}
+                            />             
+                        </div>
 
-                <div className='catalogs__item_right'>
-                    <h2>{data.attributes.name}</h2>
+                        <div className='catalogs__item_right'>
+                            <input type='text' name='name' value={data.attributes.name} placeholder='Название' className='catalogs__item_name' />
 
-                    <h3>Описание</h3>
-                    <div className='description' dangerouslySetInnerHTML={{ __html: markdown.toHTML(data.attributes.description) }}></div>
+                            <h3>Описание</h3>
+                            <textarea name="description" value={data.attributes.description} placeholder='Описание'></textarea>
 
-                    <p className='count'>Количество на складе: <b>{data.attributes.count}</b> шт.</p>
-                </div>
-            </div>
+                            <p className='count'>
+                                Количество на складе: 
+                                    <b>
+                                        <input type='text' name='count' value={data.attributes.count} placeholder='Количество' className='catalogs__item_name' />
+                                    </b>
+                                шт.
+                            </p>
+                        </div>
+                    </div>
+                ) : (
+                    <div className='catalogs__item'>
+                        <div className='catalogs__item_left'>
+                            <Image 
+                                src={process.env.NEXT_PUBLIC_STRAPI_API_URL + data.attributes.images.data[0].attributes.url} 
+                                width={600} 
+                                height={600} 
+                                alt={`${data.attributes.name}`}
+                            />             
+                        </div>
+
+                        <div className='catalogs__item_right'>
+                            <h2>{data.attributes.name}</h2>
+
+                            <h3>Описание</h3>
+                            <div className='description' dangerouslySetInnerHTML={{ __html: markdown.toHTML(data.attributes.description) }}></div>
+
+                            <p className='count'>Количество на складе: <b>{data.attributes.count}</b> шт.</p>
+                        </div>
+                    </div>
+                )
+            }
         </div>
     )
 }
