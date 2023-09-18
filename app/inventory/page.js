be8d430ed9a9d733c88896bed892dbca014a9b44
Malcev-Nikita <nik2004itamalcev@gmail.com>
2023-09-18 22:42:32 +0400

    с

--- app/inventory/page.js
+++ app/inventory/page.js
@@ -12,20 +12,14 @@ export default function Page() {
     };
 
     return (
-        <div className="App">
-            <section className="App-section">
-                <div className="App-section-title">Html5-qrcode React demo</div>
-                <br />
-                <br />
-                <br />
-                <Html5QrcodePlugin
-                    fps={10}
-                    qrbox={250}
-                    disableFlip={false}
-                    qrCodeSuccessCallback={onNewScanResult}
-                />
-                <ResultContainerPlugin results={decodedResults} />
-            </section>
+        <div className="inventory">
+            <Html5QrcodePlugin
+                fps={10}
+                qrbox={250}
+                disableFlip={false}
+                qrCodeSuccessCallback={onNewScanResult}
+            />
+            <ResultContainerPlugin results={decodedResults} />
         </div>
     );
 };
