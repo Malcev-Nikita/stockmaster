a27a104c63d92490c92c0eb550dcf32c56ef7a7b
Malcev Nikita <nik2004itamalcev@gmail.com>
2023-09-26 11:32:33 +0400

    c

--- app/auth/page.js
+++ app/auth/page.js
@@ -1,15 +1,48 @@
 "use client"
 
+import React, { useState } from 'react';
 
-export default async function Page() {
+function Auth(email, password) {
+    const emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    
+    if(email.match(emailRegex) && password.length >= 3) {
+        document.querySelector('.auth_succes').classList.add('active')
+    }
+    else {
+        document.querySelector('.auth_error').classList.add('active')
+    }
+}
+
+export default function Page() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("") 
+
     return (
         <div className='auth'>
             <h2>Авторизация</h2>
 
-            <input type="email" name="EMAIL" placeholder="Email" />
-            <input type="password" name="PASSWORD" placeholder="Пароль"/>
+            <input type="email" name="EMAIL" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+            <input type="password" name="PASSWORD" placeholder="Пароль" onChange={(e) => setPassword(e.target.value)}/>
 
-            <button>Авторизация</button>
+            <button onClick={() => Auth(email, password)}>Авторизация</button>
+
+
+
+            <div className='auth_error'>
+                <div className='fancy_close'></div>
+
+                <div className='container'>
+                    <h6>Ошибка, повторите попытку позже!</h6>
+                </div>
+            </div>
+
+            <div className='auth_succes'>
+                <div className='fancy_close'></div>
+
+                <div className='container'>
+                    <h6>Вы авторизовались в системе!</h6>
+                </div>
+            </div>
         </div>
     )
 }
