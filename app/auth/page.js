3e871393136f951835b4aa9eea55da376c1d494d
Malcev-Nikita <nik2004itamalcev@gmail.com>
2023-09-27 23:31:04 +0400

    c

--- app/auth/page.js
+++ app/auth/page.js
@@ -1,6 +1,13 @@
 "use client"
 
 import React, { useState } from 'react';
+import { useCallback } from "react";
+import Particles from "react-particles";
+import { loadSlim } from "tsparticles-slim";
+
+const options = {
+    //options
+};
 
 function Auth(email, password) {
     const emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
@@ -14,35 +21,160 @@ function Auth(email, password) {
 }
 
 export default function Page() {
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("") 
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("") 
 
-    return (
-        <div className='auth'>
-            <h2>Авторизация</h2>
+  const particlesInit = useCallback(async engine => {
+      console.log(engine);
+      await loadSlim(engine);
+  }, []);
 
-            <input type="email" name="EMAIL" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-            <input type="password" name="PASSWORD" placeholder="Пароль" onChange={(e) => setPassword(e.target.value)}/>
+  const particlesLoaded = useCallback(async container => {
+      await console.log(container);
+  }, []);
 
-            <button onClick={() => Auth(email, password)}>Авторизация</button>
+  return (
+    <div className='auth'>
+      <Particles 
+          id="tsparticles"
+          init={particlesInit}
+          loaded={particlesLoaded}
+          options={{
+            "particles": {
+              "number": {
+                "value": 110,
+                "density": {
+                  "enable": true,
+                  "value_area": 800
+                }
+              },
+              "color": {
+                "value": "#ffffff"
+              },
+              "shape": {
+                "type": "circle",
+                "stroke": {
+                  "width": 0,
+                  "color": "#000000"
+                },
+                "polygon": {
+                  "nb_sides": 5
+                },
+                "image": {
+                  "src": "img/github.svg",
+                  "width": 100,
+                  "height": 100
+                }
+              },
+              "opacity": {
+                "value": 0.1,
+                "random": false,
+                "anim": {
+                  "enable": false,
+                  "speed": 1,
+                  "opacity_min": 0.1,
+                  "sync": false
+                }
+              },
+              "size": {
+                "value": 2,
+                "random": true,
+                "anim": {
+                  "enable": false,
+                  "speed": 40,
+                  "size_min": 0.1,
+                  "sync": false
+                }
+              },
+              "line_linked": {
+                "enable": true,
+                "distance": 90,
+                "color": "#b3b3b3",
+                "opacity": 0.4,
+                "width": 1
+              },
+              "move": {
+                "enable": true,
+                "speed": 2,
+                "direction": "none",
+                "random": false,
+                "straight": false,
+                "out_mode": "out",
+                "bounce": false,
+                "attract": {
+                  "enable": false,
+                  "rotateX": 600,
+                  "rotateY": 1200
+                }
+              }
+            },
+            "interactivity": {
+              "detect_on": "canvas",
+              "events": {
+                "onhover": {
+                  "enable": true,
+                  "mode": "grab"
+                },
+                "onclick": {
+                  "enable": false,
+                  "mode": "repulse"
+                },
+                "resize": true
+              },
+              "modes": {
+                "grab": {
+                  "distance": 400,
+                  "line_linked": {
+                    "opacity": 1
+                  }
+                },
+                "bubble": {
+                  "distance": 400,
+                  "size": 40,
+                  "duration": 2,
+                  "opacity": 8,
+                  "speed": 3
+                },
+                "repulse": {
+                  "distance": 200,
+                  "duration": 0.4
+                },
+                "push": {
+                  "particles_nb": 4
+                },
+                "remove": {
+                  "particles_nb": 2
+                }
+              }
+            },
+            "retina_detect": true
+          }}
+      />
 
+      <div className='content'>
+        <h2>Авторизация</h2>
 
+        <input type="email" name="EMAIL" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" name="PASSWORD" placeholder="Пароль" onChange={(e) => setPassword(e.target.value)}/>
 
-            <div className='auth_error'>
-                <div className='fancy_close'></div>
+        <button onClick={() => Auth(email, password)}>Авторизация</button>
+      </div>
 
-                <div className='container'>
-                    <h6>Ошибка, повторите попытку позже!</h6>
-                </div>
-            </div>
+      <div className='auth_error'>
+          <div className='fancy_close'></div>
 
-            <div className='auth_succes'>
-                <div className='fancy_close'></div>
-
-                <div className='container'>
-                    <h6>Вы авторизовались в системе!</h6>
-                </div>
-            </div>
+          <div className='container'>
+              <h6>Ошибка, повторите попытку позже!</h6>
+          </div>
         </div>
-    )
+
+        <div className='auth_succes'>
+          <div className='fancy_close'></div>
+
+          <div className='container'>
+              <h6>Вы авторизовались в системе!</h6>
+          </div>
+        </div>
+    </div>
+  )
 }
--- app/components/ArrowUp.js
+++ app/components/ArrowUp.js
@@ -1,6 +1,7 @@
 'use client'
 
 import { AiOutlineArrowUp } from 'react-icons/ai'
+import React, { useEffect } from 'react';
 
 function Up() {
     window.scrollTo({
@@ -9,9 +10,30 @@ function Up() {
     })
 }
 
+function Scroll() {
+    if(self.pageYOffset > 300) {
+        document.querySelector('.arrow_up').classList.add('active')
+    }
+    else {
+        document.querySelector('.arrow_up').classList.remove('active')
+    }
+}
+
 export default function ArrowUp() {
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if(self.pageYOffset > 300) {
+                document.querySelector('.arrow_up').classList.add('active')
+            }
+            else {
+                document.querySelector('.arrow_up').classList.remove('active')
+            }
+        }
+        );
+    }, []);
+
   return (
-    <button className='arrow_up' onClick={Up}>
+    <button className='arrow_up' onClick={Up} onScroll={Scroll}>
         <AiOutlineArrowUp/>
     </button>
     )
